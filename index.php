3a9bbce13a45331dc38695a28b76cc6e4dd506ea
Samara Torquato <91499584+TorquatoSt@users.noreply.github.com>
2022-05-27 16:37:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/style-grid.css">
+    <link rel="shortcut icon" href="public/img/escola.png" type="image/x-icon">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+
+    <header>Topo</header>
+    <main>
+        <div class="center">
+        </div>
+        <div class="center">
+            <img src="public/img/escola.png" alt="">
+            <a href="./view/Aluno/index.php"><button><h2>Aluno</h2></button></a>
+            <a href="./view/Professor/index.php"><button><h2>Professor</h2></button></a>
+            <a href="./view/Fornecedor/index.php"><button><h2>Fornecedor</h2></button></a>
+        </div>
+        <div class="center">
+        </div>
+    </main>
+    
+	<asideL>Lateral ESQ</asideL>
+	<asideR>Lateral Dir</asideR>
+	<footer>Rodape </footer>
+
+
+</body>
+</html>
